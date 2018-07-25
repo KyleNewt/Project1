4c66b9690ddb3945dd4f7bb421510db6d4005d87
KyleNewt <kyle.newton.1992@gmail.com>
2018-07-25 10:17:17 -0500

    file structure, starting html/css

---
+++ project1/src/main/webapp/js/sidebar.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openMenu() {
+    document.getElementById("sideMenu").style.width = "200px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeMenu() {
+    document.getElementById("sideMenu").style.width = "0";
+}
