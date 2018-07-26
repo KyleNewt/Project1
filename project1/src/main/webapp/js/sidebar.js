90f58da728b1bc0998a1fc3b86a289ca2e3a3f74
KyleNewt <kyle.newton.1992@gmail.com>
2018-07-26 14:21:30 -0500

    Buttons with javascript

--- project1/src/main/webapp/js/sidebar.js
+++ project1/src/main/webapp/js/sidebar.js
@@ -1,9 +1,27 @@
-/* Set the width of the side navigation to 250px */
-function openMenu() {
-    document.getElementById("sideMenu").style.width = "200px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeMenu() {
-    document.getElementById("sideMenu").style.width = "0";
-}
+ 
+ function changeToFrontPage(event) {
+	    document.getElementById("mainSpace").innerHTML = "Front Page text";
+	}
+ function changeToNews(event) {
+	    document.getElementById("mainSpace").innerHTML = "News text";
+	}
+ function changeToAbout(event) {
+	    document.getElementById("mainSpace").innerHTML = "About Us text";
+	}
+ function changeToContactUs(event) {
+	    document.getElementById("mainSpace").innerHTML = "Contact Us text";
+	}
+ function changeToMyAccount(event) {
+	    document.getElementById("mainSpace").innerHTML = "My Account text";
+	}
+ function changeToViewRooms(event) {
+	    document.getElementById("mainSpace").innerHTML = "View Rooms text";
+	}
+ function changeToSubmitTicket(event) {
+	    document.getElementById("mainSpace").innerHTML = "Submit Ticket text";
+	}
+ function changeToMyInbox(event) {
+	    document.getElementById("mainSpace").innerHTML = "My Inbox text";
+	}
+ 
+ 
